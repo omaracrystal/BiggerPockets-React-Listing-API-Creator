98df5bd93bea744e02b84c9408cabf2e20294692
Crystal O'Mara <crystal.omara@investability.com>
2017-05-25 16:11:21 -0600

    setting up basic file structure and templating

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import 'normalize.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <div className="App">
+                <h1 className="App-header">Listings</h1>
+
+                <form>
+                    <input className="input-name" />
+                    <input className="input-url" />
+                    <button>ENTER</button>
+                </form>
+
+                <ul>
+                    <li>
+                        <h3>Dolor Ipsum</h3>
+                        <p>www.something.com</p>
+                    </li>
+                    <li>
+                        <h3>Dolor Ipsum</h3>
+                        <p>www.something.com</p>
+                    </li>
+                    <li>
+                        <h3>Dolor Ipsum</h3>
+                        <p>www.something.com</p>
+                    </li>
+                    <li>
+                        <h3>Dolor Ipsum</h3>
+                        <p>www.something.com</p>
+                    </li>
+                    <li>
+                        <h3>Dolor Ipsum</h3>
+                        <p>www.something.com</p>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default App;
